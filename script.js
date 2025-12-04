1a973bd7c6626e0c9280d3cebe82f7f134278c57
Savvy Digital <info@savvydigital.id>
2025-12-04 12:35:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// LOGIC JAVASCRIPT FOFo V1.0
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("FoFo JavaScript siap dimuat!");
+    // Semua logic LocalStorage akan kita taruh di sini!
+});
